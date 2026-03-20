1484b2c44a714c594d22f50fbd06054f9034b036
Joseph Ayitey <joseph.yaw.ayitey@gmail.com>
2026-03-20 22:37:55 +0100

    I have added an answer file and answers the questions

---
+++ number-systems/Answers-01.sh
@@ -0,0 +1,37 @@
+Convert the decimal number 14 to binary. Answer: 14 = 8 + 4 + 2 = 1110.
+
+Convert the binary number 101101 to decimal: Answer: 101101 = 32 + 8 + 4 + 1 = 45
+
+Which is larger: 1000 or 0111? Answer: 1000 = (1000(8)>0111(7))
+
+Which is larger: 00100 or 01011? Answer: 01011
+
+What is 10101 + 01010? Answer: 11111
+
+What is 10001 + 10001? Answer: 100010
+
+What's the largest number you can store with 4 bits, if you want to be able to represent the number 0? Answer: With 4 bits: 0000 to 1111. Largest is 1111 = 15.
+
+How many bits would you need in order to store the numbers between 0 and 255 inclusive? Answer: 8 bits = 256 values total. 2^8 = 256.  
+
+How many bits would you need in order to store the numbers between 0 and 3 inclusive? Answer: 2 bits =  4 values. 2^2 = 4. 
+How many bits would you need in order to store the numbers between 0 and 1000 inclusive? Answer:10 bits = 1001 values. 2^10 = 1024 > 1001.
+
+How can you test if a binary number is a power of two (e.g. 1, 2, 4, 8, 16, ...)? Answer: A power of two in binary has exactly one bit set to 1:
+
+Convert the decimal number 14 to hex. Answer: E
+
+Convert the decimal number 386 to hex. Answer: 182
+
+Convert the hex number 386 to decimal. Answer: 902
+
+Convert the hex number B to decimal. Answer: 11
+
+If reading the byte 0x21 as a number, what decimal number would it mean? Answer: 33
+
+If reading the byte 0x21 as an ASCII character, what character would it mean? Answer: !
+
+If reading the byte 0x21 as a greyscale colour, as described in "Approaches for Representing Colors and Images", what colour would it mean? Answer: 0x21 = 33 out of 255. Very dark grey 
+If reading the bytes 0xAA00FF as an RGB colour, as described in "Approaches for Representing Colors and Images", what colour would it mean? Answer: R=0xAA (170), G=0x00 (0), B=0xFF (255). Purple/magenta 
+
+If reading the bytes 0xAA00FF as a sequence of three one-byte decimal numbers, what decimal numbers would they be? Answer: 170, 0, 255
