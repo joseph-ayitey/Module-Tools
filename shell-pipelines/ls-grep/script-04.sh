9f1f1d7ae9c6bdecbad0143cb1223330a90ff5e1
Joseph Ayitey <joseph.yaw.ayitey@gmail.com>
2026-04-09 15:14:41 +0200

    Update script-04.sh to count files starting with an upper case letter using ls and grep

--- shell-pipelines/ls-grep/script-04.sh
+++ shell-pipelines/ls-grep/script-04.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 # TODO: Write a command to count the number of files in the sample-files directory whose name starts with an upper case letter and doesn't contain any other upper case letters.
 # Your output should be the number 7.
-find sample-files -maxdepth 1 -type f -regex 'sample-files/[A-Z][^A-Z]*' | wc -l
+ls sample-files | grep '^[A-Z][^A-Z]*$' | wc -l
